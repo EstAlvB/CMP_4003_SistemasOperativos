e9f93e79b4e0732cfff32de0793e34388d971227
gapastorv <125485483+gapastorv@users.noreply.github.com>
2023-03-27 10:50:30 -0500

    Update TraderImpl.java
    Se añadió librerías de concurrent, el semaphore puede que sea un problema pero lo queda ahí por ahora, se añade codigo a los primeros tres métodos. Aún falta ver que se debería hacer en el resto.

--- Project2-Synchronization/TraderImpl.java
+++ Project2-Synchronization/TraderImpl.java
@@ -1,16 +1,40 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class TraderImpl implements Trader {
-    public TraderImpl(Grain specialty) {
 
+   private Grain specialityGrain;
+   private Order stock;
+   private int surplus = 0;
+   private final int maxWait = 50;// true, so locks favor granting access to the longest-waiting thread
+   private final ReentrantLock lock = new ReentrantLock(true);
+   private final ReentrantLock stockLock = new ReentrantLock(true);
+
+    public TraderImpl(Grain specialty) {
+      this.specialityGrain = specialty;
+      stock = new Order();
     }
 
     public Order getAmountOnHand() {
-        return new Order();
+        //getAmountOnHand_String();
+        //System.out.println(getAmountOnHand_String());
+        return stock;
     }
 
     public void get(Order order) throws InterruptedException {
-
+         //printEverybodyStock();
+         while (lock.isLocked()) {
+            Thread.sleep(100);
+         }lock.lock();
+    /*     try {
+            System.out.println("Order ");
+            int timesWaiting = 0;
+            while (stock.get(this.specialityGrain)< order.get(this.specialityGrain)) {
+            //System.out.print("Order " + order.get(this.specialityGrain)
+            //
+            */
     }
 
     public void swap(Grain what, int amt) throws InterruptedException {
@@ -20,5 +44,6 @@ public class TraderImpl implements Trader {
     public void deliver(int amt) throws InterruptedException {
 
     }
+    
 }
 
