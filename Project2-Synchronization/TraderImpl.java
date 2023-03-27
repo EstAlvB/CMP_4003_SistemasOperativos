d6393d6d044b99cd0169f48032eba39921f9e6db
Esteban Alvarado <89538113+EstAlvB@users.noreply.github.com>
2023-03-27 10:35:31 -0500

    Java Files added

---
+++ Project2-Synchronization/TraderImpl.java
@@ -0,0 +1,24 @@
+
+
+public class TraderImpl implements Trader {
+    public TraderImpl(Grain specialty) {
+
+    }
+
+    public Order getAmountOnHand() {
+        return new Order();
+    }
+
+    public void get(Order order) throws InterruptedException {
+
+    }
+
+    public void swap(Grain what, int amt) throws InterruptedException {
+
+    }
+
+    public void deliver(int amt) throws InterruptedException {
+
+    }
+}
+
